76ed569b77581cbb872795585cdead920258c51d
SoLuutions <153301308+SoLuutions@users.noreply.github.com>
2024-06-11 04:14:57 +0800

    Update sw.js

--- dist/sw.js
+++ dist/sw.js
@@ -1,23 +1,8 @@
 // sw.js
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png'
-      ]);
-    })
-  );
+  console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
 });
