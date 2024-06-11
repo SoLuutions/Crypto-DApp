d898cf52ff10991cf9f3a9b7dd9c7358631a4dd0
SoLuutions <153301308+SoLuutions@users.noreply.github.com>
2024-06-11 23:41:47 +0800

    Add files via upload

---
+++ dist/service-worker.js
@@ -0,0 +1,31 @@
+const cacheName = 'trending-coins-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
